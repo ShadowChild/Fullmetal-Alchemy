99ab074cbc0bef551484c0f93aeea176d095cdd5
Zach Piddock <shadychild12@gmail.com>
2018-03-11 18:38:51 +0000

    Added a tooltip

--- src/main/java/io/github/shadowchild/fma/content/item/ItemPStone.java
+++ src/main/java/io/github/shadowchild/fma/content/item/ItemPStone.java
@@ -2,8 +2,13 @@ package io.github.shadowchild.fma.content.item;
 
 
 import io.github.shadowchild.fma.content.ItemBase;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemPStone extends ItemBase {
 
@@ -26,5 +31,12 @@ public class ItemPStone extends ItemBase {
     public EnumRarity getRarity(ItemStack par1ItemStack) {
 
         return isFake ? EnumRarity.UNCOMMON : EnumRarity.EPIC;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+
+        tooltip.add("A stone which is said to break the law of exchange!");
+        if(isFake) tooltip.add("Something doesn't seem quite right with this Stone...");
     }
 }
